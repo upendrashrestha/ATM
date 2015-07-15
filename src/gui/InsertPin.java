2e91a63486f2aaf1ac94f242b92b88d8a60002e2
Upendra Shrestha <upendra.shrestha1@gmail.com>
2015-07-15 14:58:46 -0500

    Chamges from Naren

--- src/gui/InsertPin.java
+++ src/gui/InsertPin.java
@@ -1,4 +1,4 @@
-package gUI;
+package gui;
 
 import java.awt.EventQueue;
 
--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,7 +1,8 @@
-package gUI;
+package gui;
 
 import java.awt.EventQueue;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.BorderLayout;
@@ -12,12 +13,16 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
 import java.awt.SystemColor;
+import java.awt.Window;
+
 import javax.swing.JTextField;
 
 public class Main {
 
 	private JFrame frame;
 	private JTextField textField;
+	
+	private InsertPin insertpin;
 
 	/**
 	 * Launch the application.
@@ -47,31 +52,41 @@ public class Main {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.getContentPane().setBackground(SystemColor.textHighlight);
+		frame.getContentPane().setBackground(new Color(0, 102, 102));
 		frame.getContentPane().setFont(new Font("Tahoma", Font.PLAIN, 20));
 		frame.setBounds(100, 100, 600, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		frame.setResizable(false);
 		
 		JLabel lblNewLabel = new JLabel("WELCOME TO RANGO BANK");
+		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setBackground(Color.WHITE);
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		lblNewLabel.setFont(new Font("Arial Black", Font.BOLD, 30));
 		lblNewLabel.setBounds(10, 11, 564, 34);
 		frame.getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Please Insert Card");
+		lblNewLabel_1.setForeground(Color.WHITE);
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_1.setBounds(10, 115, 564, 34);
 		frame.getContentPane().add(lblNewLabel_1);
 		
 		JButton btnNewButton = new JButton("Insert");
-		btnNewButton.setForeground(new Color(0, 0, 0));
+		btnNewButton.setForeground(Color.WHITE);
+		btnNewButton.setOpaque(true);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				
+				frame.dispose(); //closes the window--cannot be recovered
+				insertpin=new InsertPin();
+				insertpin.NewScreen();
+				
 			}
 		});
-		btnNewButton.setBackground(new Color(0, 128, 128));
+		btnNewButton.setBackground(new Color(0, 0, 153));
 		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btnNewButton.setBounds(197, 281, 200, 50);
 		frame.getContentPane().add(btnNewButton);
@@ -82,5 +97,10 @@ public class Main {
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		textField.setDocument(new LimitDocument(16));
+		
+		JLabel lblNewLabel_2 = new JLabel(new ImageIcon("C:\\Users\\Dell\\Documents\\GitHub\\ATM\\src\\insert.png"));
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setBounds(33, 160, 154, 138);
+		frame.getContentPane().add(lblNewLabel_2);
 	}
 }
