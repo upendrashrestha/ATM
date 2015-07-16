bfe5eb9d0309733175179eb08a5d400b70dd7dcd
narenbruce@hotmail.com <narenbruce@hotmail.com>
2015-07-15 22:20:44 -0500

    balance

--- src/gui/DisplayBalance.java
+++ src/gui/DisplayBalance.java
@@ -3,6 +3,10 @@ package gui;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
 
 public class DisplayBalance {
 
@@ -36,8 +40,43 @@ public class DisplayBalance {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.getContentPane().setBackground(new Color(0, 102, 102));
+		frame.setBounds(100, 100, 600, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Balance Details");
+		lblNewLabel.setFont(new Font("Arial Black", Font.BOLD, 30));
+		lblNewLabel.setForeground(new Color(255, 255, 255));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(10, 60, 564, 62);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Account:");
+		lblNewLabel_1.setFont(new Font("Arial Black", Font.PLAIN, 20));
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNewLabel_1.setForeground(new Color(255, 255, 255));
+		lblNewLabel_1.setBounds(10, 174, 215, 50);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Total Balance:");
+		lblNewLabel_2.setFont(new Font("Arial Black", Font.PLAIN, 20));
+		lblNewLabel_2.setForeground(new Color(255, 255, 255));
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNewLabel_2.setBounds(10, 235, 215, 40);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("account type");
+		lblNewLabel_3.setForeground(new Color(255, 255, 255));
+		lblNewLabel_3.setFont(new Font("Arial Black", Font.PLAIN, 20));
+		lblNewLabel_3.setBounds(264, 174, 215, 50);
+		frame.getContentPane().add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("total balance");
+		lblNewLabel_4.setFont(new Font("Arial Black", Font.PLAIN, 20));
+		lblNewLabel_4.setForeground(new Color(255, 255, 255));
+		lblNewLabel_4.setBounds(264, 235, 215, 40);
+		frame.getContentPane().add(lblNewLabel_4);
 	}
 
 }
