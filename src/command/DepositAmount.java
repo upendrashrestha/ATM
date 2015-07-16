144804939dd8bf7e61fe864812e261b7b131acb4
sajala-maharjan <maharjansajala@gmail.com>
2015-07-15 20:29:37 -0500

    DepositAmount command.

--- src/command/DepositAmount.java
+++ src/command/DepositAmount.java
@@ -1,11 +1,27 @@
 package command;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import visitor.MoneyElement;
+import visitor.MoneyParser;
+
 public class DepositAmount implements ICommand{
 
+	private MoneyParser moneyParser;
+	private double amount;
+	
+	public DepositAmount(MoneyParser moneyParser, double amount) {
+
+		this.moneyParser = moneyParser;
+		this.amount = amount;
+	}
+
 	@Override
-	public boolean execute() {
-		// TODO Auto-generated method stub
-		return false;
+	public void execute() {
+		Map<MoneyElement, Integer> money = new HashMap<MoneyElement, Integer>();
+		
+		moneyParser.calculateMoney(money);
 	}
 
 	@Override
