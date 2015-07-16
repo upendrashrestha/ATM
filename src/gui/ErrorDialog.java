1e182b74975c6c1a83f473bef5185d8c69b5c451
narenbruce@hotmail.com <narenbruce@hotmail.com>
2015-07-15 22:22:33 -0500

    erro

--- src/gui/ErrorDialog.java
+++ src/gui/ErrorDialog.java
@@ -9,6 +9,7 @@ import javax.swing.JLabel;
 import java.awt.BorderLayout;
 import javax.swing.SwingConstants;
 import java.awt.Font;
+import javax.swing.Icon;
 
 public class ErrorDialog {
 
@@ -46,18 +47,22 @@ public class ErrorDialog {
 		frame.getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Error");
-		lblNewLabel.setBounds(10, 11, 564, 97);
+		lblNewLabel.setBounds(273, 55, 170, 49);
 		lblNewLabel.setForeground(new Color(255, 255, 255));
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Arial Black", Font.PLAIN, 30));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.LEFT);
 		frame.getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Error Message Here");
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_1.setForeground(new Color(255, 255, 255));
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblNewLabel_1.setBounds(10, 181, 564, 61);
+		lblNewLabel_1.setBounds(10, 134, 564, 61);
 		frame.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel(new ImageIcon("C:\\Users\\Dell\\Desktop\\errors.png"));
+		lblNewLabel_2.setBounds(176, 42, 128, 81);
+		frame.getContentPane().add(lblNewLabel_2);
 		frame.setBounds(100, 100, 600, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
